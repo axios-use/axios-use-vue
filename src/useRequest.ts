7f9b3302d66a239a33a502721a594a865f3eadcf
wangcch <wangcch.cc@gmail.com>
2023-03-02 23:42:38 +0800

    feat: useRequest

---
+++ src/useRequest.ts
@@ -0,0 +1,94 @@
+import type { ComputedRef } from "vue";
+import { computed, ref, unref, onUnmounted } from "vue";
+import type {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  Canceler,
+  CancelToken,
+  CancelTokenSource,
+} from "axios";
+import axios from "axios";
+
+import { getUseRequestConfig } from "./context";
+
+import type {
+  RequestFactory,
+  RequestCallbackFn,
+  Request,
+  Payload,
+  BodyData,
+} from "./request";
+import { createRequestError } from "./request";
+
+export type UseRequestOptions<T extends Request> = RequestCallbackFn<T> & {
+  instance?: AxiosInstance;
+};
+
+export type UseRequestResult<T extends Request> = [
+  RequestFactory<T>,
+  {
+    hasPending: ComputedRef<boolean>;
+    clear: Canceler;
+  },
+];
+
+export default function useRequest<T extends Request>(
+  fn: T,
+  options?: UseRequestOptions<T>,
+): UseRequestResult<T> {
+  const requestConfig = getUseRequestConfig();
+  const _axiosIns = options?.instance || requestConfig.instance;
+
+  const sources = ref<CancelTokenSource[]>([]);
+  const hasPending = computed(() => unref(sources).length > 0);
+  const { onCompleted, onError } = options || {};
+
+  const removeCancelToken = (token: CancelToken) => {
+    const _sources = unref(sources);
+    if (token && _sources.length > 0) {
+      sources.value = _sources.filter((s) => s.token !== token);
+    }
+  };
+
+  const clear = (message?: string) => {
+    const _sources = unref(sources);
+    if (_sources.length > 0) {
+      _sources.map((s) => s.cancel(message));
+      sources.value = [];
+    }
+  };
+
+  onUnmounted(() => {
+    clear();
+  });
+
+  const request = (...args: Parameters<T>) => {
+    const _config = fn(...args);
+    const _source = axios.CancelToken.source();
+
+    const ready = () => {
+      sources.value = [...unref(sources), _source];
+
+      return _axiosIns({ ..._config, cancelToken: _source.token })
+        .then((res: AxiosResponse<Payload<T>, BodyData<T>>) => {
+          removeCancelToken(_source.token);
+
+          onCompleted?.(res.data, res);
+          return [res.data, res] as const;
+        })
+        .catch((err: AxiosError<Payload<T>, BodyData<T>>) => {
+          removeCancelToken(_source.token);
+
+          const _error = createRequestError(err);
+          onError?.(_error);
+
+          throw _error;
+        });
+    };
+
+    return { ready, cancel: _source.cancel };
+  };
+
+  return [request, { clear, hasPending }];
+}
