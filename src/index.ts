307a6e0fa38498edfe1610c639cf1955c2f28ccf
wangcch <wangcch.cc@gmail.com>
2023-03-03 14:56:02 +0800

    feat: export hooks

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import plugin from "./plugin";
+
+export type { RequestConfigType } from "./context";
+
+export * from "./request";
+export * from "./useRequest";
+export * from "./useResource";
+
+export default plugin;
